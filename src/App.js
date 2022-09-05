bb0cd133c54b632ea5a4c4100c5a9fcbc7435688
Diniki <jayakodyjadn.19@uom.lk>
2022-09-05 09:09:36 +0530

    Edit Room added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Screens/HomePage";
 import RoomHome from "./Screens/RoomHome";
 import AddRoom from "./Screens/AddRoom";
-import ViewRoom from "./Screens/AddRoom";
+import ViewRoom from "./Screens/ViewRoom";
+import EditRoom from "./Screens/EditRoom";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path='/room_home' element={ <RoomHome/>}/>
           <Route path='/add_room' element={ <AddRoom/>}/>
           <Route path='/view_room' element={ <ViewRoom/>}/>
+          <Route path='/edit_room' element={ <EditRoom/>}/>
         </Routes>
       </Router>   
     </div>
--- src/Screens/RoomHome.jsx
+++ src/Screens/RoomHome.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-import { Link, Navigate } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { message, Popconfirm, Button } from 'antd';
+import {DeleteOutlined,QuestionCircleOutlined} from '@ant-design/icons';
 
 import RoomHomeStyles from './RoomHome.module.css';
 import NavBar from '../Common_Components/NavBar';
+
+
+const confirm = (e) => {
+    console.log(e);
+    message.success('Click on Yes');
+    
+  };
+  
+  const cancel = (e) => {
+    console.log(e);
+    message.error('Click on No');
+  };
+
 
 function RoomHome()
 {
@@ -13,7 +28,7 @@ function RoomHome()
         <>
             <div>
                 <NavBar/>
-                <div style={{height:'auto', width:'65vw', marginLeft:'30vw'}}>
+                <div style={{height:'auto', width:'65vw', marginLeft:'26vw'}}>
                     <div>
                         <button style={{float:'right'}}>Generate Report</button>
                         <div style={{height:'8vw'}}></div>
@@ -24,12 +39,16 @@ function RoomHome()
                     <div>
                         <center>
                         <table>
+                           
                             <tr className={RoomHomeStyles.tableRow}>
                                 <th className={RoomHomeStyles.tableHeader}>Room Code</th>
                                 <th className={RoomHomeStyles.tableHeader}>Room Type</th>
                                 <th className={RoomHomeStyles.tableHeader}>Sleeps</th>
                                 <th className={RoomHomeStyles.tableHeader}>Price</th>
                                 <th className={RoomHomeStyles.tableHeader}>Image</th>
+                                <th className={RoomHomeStyles.tableHeader}></th>
+                                <th className={RoomHomeStyles.tableHeader}></th>
+                                <th className={RoomHomeStyles.tableHeader}></th>
                             </tr>
                             
                             <tr className={RoomHomeStyles.tableRow}>
@@ -38,6 +57,23 @@ function RoomHome()
                                 <td className={RoomHomeStyles.tableHeader}>2</td>
                                 <td className={RoomHomeStyles.tableHeader}>23000</td>
                                 <td className={RoomHomeStyles.tableHeader}>Image</td>
+                                <Link to='/view_room'> <td className={RoomHomeStyles.tableHeader}>{">>"}</td></Link>  
+                                <td className={RoomHomeStyles.tableHeader}>
+                                <Link to='/edit_room'> <button>Edit</button></Link>
+                                </td>
+                                <Popconfirm title="Are you sure want to Delete this category?"
+              // onConfirm={()=>{endCategory({Text})}}
+              onCancel={null}
+              okText="Yes"
+              cancelText="No"
+              icon={<QuestionCircleOutlined/>}>
+         <Button 
+          icon={<DeleteOutlined style={{float:'right',color:'black'}}/>}
+          style={{float:'right',  backgroundColor:'#ff7875'}}
+          // onClick={endCategory()}
+          > 
+          </Button>
+            </Popconfirm>
                             </tr>
                             
 
