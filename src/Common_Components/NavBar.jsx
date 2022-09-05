701d9387ea8a1ed71dc66d88ef1357d95a9670b8
savi3307 <it20636074@my.sliit.lk>
2022-09-05 07:14:46 +0530

    Navbar Icons added

--- src/Common_Components/NavBar.jsx
+++ src/Common_Components/NavBar.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import food from "../Resources/food.png";
+import customer from "../Resources/Cus.png";
+import employee from "../Resources/Emp.png";
+import payment from "../Resources/Payment.png";
+import room from "../Resources/Room.png";
+import addRooom from "../Resources/Room plus.png";
 
 import NavBarStyles from './NavBar.module.css';
 
@@ -8,12 +14,12 @@ function NavBar()
         <>
             <div className={NavBarStyles.box}>
                 <table>
-                    <tr><button className={NavBarStyles.navBtn}>Foods</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Rooms</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Room Bookings</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Employees</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Payments</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Customers</button></tr>
+                <tr><td><button className={NavBarStyles.navBtn}><img src={food} width="30" height="30"/>Foods</button></td></tr>
+                    <tr><td><button className={NavBarStyles.navBtn}><img src={room} width="30" height="30"/>Rooms</button></td></tr>
+                    <tr><td><button className={NavBarStyles.navBtn}><img src={addRooom} width="30" height="30"/> Bookings      </button></td></tr>
+                    <tr><td><button className={NavBarStyles.navBtn}><img src={employee} width="30" height="30"/>Employees</button></td></tr>
+                    <tr><td><button className={NavBarStyles.navBtn}><img src={payment} width="30" height="30"/>Payments</button></td></tr>
+                    <tr><td><button className={NavBarStyles.navBtn}><img src={customer} width="30" height="30"/>Customers</button></td></tr>
                 </table>
             </div>
         </>
