3c5be538999f3c2d4603659d0672214fb773dac9
Diniki <jayakodyjadn.19@uom.lk>
2022-09-06 10:25:52 +0530

    Footer design -M

--- src/Common_Components/Footer.jsx
+++ src/Common_Components/Footer.jsx
@@ -10,12 +10,24 @@ function Footer()
                 <center>
                 <table>
                     <row>
-                        <td className={FooterStyles.footerContent}>LOGO</td>
+                        <td className={FooterStyles.footerContent}>
+                            <h3>Logo</h3>
+                            <p className={FooterStyles.footerData}>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+                            </p>
+                        </td>
                         <td className={FooterStyles.footerContent}>
                             <h3>COMPANY</h3>
+                            <p className={FooterStyles.footerData}>About Us</p>
+                            <p className={FooterStyles.footerData}>Legal Information</p>
+                            <p className={FooterStyles.footerData}>Contact Us</p>
+                            <p className={FooterStyles.footerData}>Blogs</p>
                         </td>
                         <td className={FooterStyles.footerContent}>
                             <h3>CONTACT INFO</h3>
+                            <p className={FooterStyles.footerData}>Phone: 0112222783</p>
+                            <p className={FooterStyles.footerData}>Email: anniston@gmail.com</p>
+                            <p className={FooterStyles.footerData}>Address: 100 Smart Street, Colombo, Sri Lanka.</p>
                         </td>
                     </row>
                 </table>
